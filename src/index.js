6b095def453aa99130ac39c86d14c9f7500bb2c3
Michael Fryer <michael.d.fryer@gmail.com>
2021-07-27 15:18:02 -0700

    Creating the board with loops

--- src/index.js
+++ src/index.js
@@ -10,34 +10,36 @@ function Square(props) {
   );
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    )
-  }
+function BoardRow(props) {
+  return (
+    <div className="board-row">
+      {props.items}
+    </div>
+  );
+}
 
+class Board extends React.Component {
   render() {
+    let rows = Array(3).fill(null);
+    for (let i = 0; i < 3; ++i)
+    {
+      let squares = Array(3).fill(null);
+      for (let j = 0; j < 3; ++j)
+      {
+        const val = (i * 3) + j;
+        squares[j] = Square({
+          value: this.props.squares[val],
+          onClick: () => this.props.onClick(val),
+        });
+      }
+      rows[i] = BoardRow({
+        items: squares,
+      });
+    }
+
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {rows}
       </div>
     );
   }
