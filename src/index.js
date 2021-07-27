b2e8deccbad76965686f5be8f5a265e9118329f6
Michael Fryer <michael.d.fryer@gmail.com>
2021-07-27 13:14:21 -0700

    Converting Square component to a render function

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick={() => props.onClick()}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -34,6 +32,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     alert("DinkDonk");
+    const squares = this.states.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
   }
 
   render() {
