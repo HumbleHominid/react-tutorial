44e788d45e39c1af1e21c0d9ba5201779b0cf0fd
Michael Fryer <michael.d.fryer@gmail.com>
2021-07-27 13:34:28 -0700

    Handling turns correctly

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
@@ -31,14 +32,16 @@ class Board extends React.Component {
   }
 
   handleClick(i) {
-    alert("DinkDonk");
-    const squares = this.states.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
