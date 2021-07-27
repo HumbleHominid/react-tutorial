63ee08e14ea59ae161b29bdf5655ebbf3574e025
Michael Fryer <michael.d.fryer@gmail.com>
2021-07-27 15:39:59 -0700

    Refactoring calculateWinner to return a winner object

--- src/index.js
+++ src/index.js
@@ -78,7 +78,7 @@ class Game extends React.Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
+    if (calculateWinner(squares).winner || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
@@ -94,7 +94,7 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+    const winnerObj = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
       const desc = move ?
@@ -108,8 +108,8 @@ class Game extends React.Component {
     });
 
     let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
+    if (winnerObj.winner) {
+      status = 'Winner: ' + winnerObj.winner;
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
@@ -144,14 +144,21 @@ function calculateWinner(squares) {
     [ 2, 4, 6 ],
   ];
 
+  let ret = {
+    winner: null,
+    winningLine: null,
+  };
+
   for (let i = 0; i < lines.length; ++i) {
     const [ a, b, c ] = lines[i];
 
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      ret.winner = squares[a];
+      ret.winningLine = lines[i];
+      break;
     }
   }
-  return null;
+  return ret;
 }
 
 // ========================================
